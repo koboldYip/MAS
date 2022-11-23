578ed9a1bce742eae81a37f1a9b84bed746ff7a0
koboldYip <kyldoks@gmail.com>
2022-11-24 02:34:48 +0300

    Lab3 Add class StartSearch.java, new logical start

---
+++ ShortestPathGraph/src/main/java/ru/mpei/behaviours/StartSearch.java
@@ -0,0 +1,32 @@
+package ru.mpei.behaviours;
+
+import jade.core.behaviours.WakerBehaviour;
+import lombok.Getter;
+import lombok.Setter;
+import ru.mpei.common.Content;
+import ru.mpei.common.NodeAgent;
+import ru.mpei.common.config.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+public class StartSearch extends WakerBehaviour {
+
+    private Configuration configuration;
+
+    public StartSearch(NodeAgent a, long timeout, Configuration configuration) {
+        super(a, timeout);
+        this.configuration = configuration;
+    }
+
+    @Override
+    protected void onWake() {
+        List<Content> successfulPaths = new ArrayList<>();
+        List<Content> failedPaths = new ArrayList<>();
+        getAgent().addBehaviour(new ReceiveBackwardRequest(configuration, successfulPaths, failedPaths));
+        getAgent().addBehaviour(new CalculateResult(getAgent(), 1000, configuration, successfulPaths, failedPaths));
+        getAgent().addBehaviour(new SendFirstRequest(getAgent(), 500, configuration));
+    }
+}
